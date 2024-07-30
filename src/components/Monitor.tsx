6bb5dfe1d43f8f7f6eb88da767e9efc6277c60ac
Jayce Meyer <jnmeyer@nps.gov>
2024-07-30 12:06:12 -0600

    updates

--- src/components/Monitor.tsx
+++ src/components/Monitor.tsx
@@ -4,15 +4,13 @@ import './styles/Monitor.css'
 export function Monitor() {
     return (
         <div id="monitor" className="on" style={{width: '100%'}}>
-            <div className='texture-overlay'></div>
                 <div id="screen">
                     <div id="crt">
-                        {/* <div className='lines-overlay'></div>
-                        <div className='dots-overlay'></div> */}
-                        {/* <Actions></Actions> */}
                         <div className="scanline"></div>
-                        <div className="terminal"></div>
-                        {/* {children} */}
+                        <div className="terminal">
+
+                            <Actions></Actions>
+                        </div>
                     </div>
                 </div>
         </div>
